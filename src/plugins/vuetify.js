cf03b0a3f72162a73ca8a276fbaccfecd78fd7bf
Christian Grossmüller <chgad.games@gmail.com>
2024-04-25 16:14:43 +0200

    ADd same theme as in clock.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,15 +5,36 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
-  },
-})
+    defaultTheme: "light",
+    variations: {
+      colors: ["primary"],
+      darken: 4,
+      lighten: 4
+    },
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          accent: "#82B1FF",
+          error: "#FF5252",
+          info: "#2196F3",
+          primary: "#1976D2",
+          secondary: "#424242",
+          success: "#4CAF50",
+          warning: "#FB8C00",
+          footerPrimary: "#0064BAFF",
+          footerSecondary: "#51B0FFFF"
+        }
+      }
+    }
+  }
+});
