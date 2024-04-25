e4c064af775e974d9f9952a8070f832f7f744f9e
Christian Grossmüller <chgad.games@gmail.com>
2024-04-25 18:48:18 +0200

    Implement Overlay for loading.

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@
       <v-container style="height: 100%">
         <router-view />
       </v-container>
+      <v-overlay :model-value="store.getters.isLoading" persistent></v-overlay>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-//
+import { useStore } from "vuex";
+const store = useStore();
 </script>
