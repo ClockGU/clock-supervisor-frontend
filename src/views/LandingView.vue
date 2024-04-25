8abfbde20478c305a797062e9a3dbfff9d5006fd
Christian Grossmüller <chgad.games@gmail.com>
2024-04-25 18:48:29 +0200

    ADd Login functionality to login button.

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -9,8 +9,20 @@
           <h2>Willkommen im Vorgesetztenportal von CLOCK</h2>
         </v-card-text>
         <v-card-actions>
-          <v-spacer></v-spacer><v-btn variant="elevated"> Zum Login </v-btn
-          ><v-spacer></v-spacer>
+          <v-spacer />
+          <v-tooltip
+            location="bottom"
+            :model-value="hasError"
+            @update:model-value=""
+          >
+            <template #activator="{ props }">
+              <v-btn v-bind="props" variant="elevated" @click="login">
+                Zum Login
+              </v-btn>
+            </template>
+            <span style="color: black"> {{ error }}</span>
+          </v-tooltip>
+          <v-spacer />
         </v-card-actions>
       </v-card>
     </v-col>
@@ -19,6 +31,34 @@
 
 <script setup>
 import ClockIcon from "@/components/logo/ClockIcon.vue";
+import { useStore } from "vuex";
+import ApiService from "@/services/api";
+import { computed, ref } from "vue";
+const store = useStore();
+const error = ref("");
+
+const hasError = computed(() => error.value !== "");
+async function login() {
+  await store.dispatch("setIsLoading");
+  try {
+    const response = ApiService.get(
+      `/auth/o/authorize/?redirect_uri=${import.meta.env.VITE_PUBLIC_URL}/login`
+    );
+    const { authorization_url } = response.data;
+    window.location = authorization_url;
+
+    setTimeout(() => store.dispatch("unsetLoading"), 5000);
+  } catch (e) {
+    await store.dispatch("unsetLoading");
+    error.value =
+      "Ein Fehler ist aufgetreten. Bitte kontaktieren Sie den Support.";
+    setTimeout(() => (error.value = ""), 5000);
+  }
+}
 </script>
 
-<style scoped lang="sass"></style>
+<style scoped lang="scss">
+.v-tooltip :deep(.v-overlay__content) {
+  background-color: rgb(var(--v-theme-error));
+}
+</style>
