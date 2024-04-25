68a683b73f8c1222d22728c1fc8cbcba12bd2a70
Christian Grossmüller <chgad.games@gmail.com>
2024-04-25 12:39:41 +0200

    Add two standard routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-
 /**
  * router/index.ts
  *
@@ -7,10 +6,17 @@
 
 // Composables
 import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "@/views/LoginView.vue";
+import ContentView from "@/views/ContentView.vue";
 
+const routes = [
+  { path: "/", component: LoginView },
+  { path: "/main", component: ContentView }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-})
+  routes: routes
+});
 
-export default router
+export default router;
