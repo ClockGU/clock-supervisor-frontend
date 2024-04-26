67992050a3064c4094aaf21fc65caad0a7212a85
Christian Grossmüller <chgad.games@gmail.com>
2024-04-26 14:11:56 +0200

    Implement basic Login process.

--- src/store/modules/general.js
+++ src/store/modules/general.js
@@ -1,18 +1,39 @@
+import ApiService from "@/services/api";
+
 const state = () => ({
-  loading: false
+  loading: false,
+  accessToken: undefined,
+  refreshToken: undefined,
+  user: undefined
 });
 
 const getters = {
-  isLoading: (state) => state.loading
+  isLoading: (state) => state.loading,
+  accessToken: (state) => state.accessToken,
+  refreshToken: (state) => state.refreshToken
 };
 
 const mutations = {
-  setLoading: (state, value) => (state.loading = value)
+  setLoading: (state, value) => (state.loading = value),
+  setAccessToken: (state, value) => (state.accessToken = value),
+  setRefreshToken: (state, value) => (state.refreshToken = value),
+  unsetTokens: (state) => {
+    state.accessToken = undefined;
+    state.refreshToken = undefined;
+  },
+  setUser: (state, value) => (state.user = value)
 };
 
 const actions = {
   setIsLoading: ({ commit }) => commit("setLoading", true),
-  unsetLoading: ({ commit }) => commit("setLoading", false)
+  unsetLoading: ({ commit }) => commit("setLoading", false),
+  login: ({ commit }, payload) => {
+    commit("setAccessToken", payload.access_token);
+    commit("setRefreshToken", payload.refresh_token);
+    ApiService.setAccessToken(payload.access_token);
+  },
+  logout: ({ commit }) => commit("unsetTokens"),
+  setUser: ({ commit }, payload) => commit("setUser", payload)
 };
 
 export default {
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,12 +2,50 @@
   <v-row align="center" justify="center" style="height: 100%">
     <v-col class="d-flex" cols="6">
       <v-card>
-        <h1>SUCCESS!!</h1>
+        <h1>SUCCESS LOGIN PAGE!!</h1>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import ApiService from "@/services/api";
+import { useStore } from "vuex";
+
+const store = useStore();
+const router = useRouter();
+const route = useRoute();
+
+onMounted(async () => {
+  await store.dispatch("setIsLoading");
+  const { code } = route.query;
+  // Replace the history entry to remove the auth code form the browser address bar
+  window.history.replaceState({}, null, "/");
+  try {
+    const response = await ApiService.post("/supervisor/auth/o/token/", {
+      code
+    });
+    await store.dispatch("login", response.data);
+  } catch {
+    // TODO: Specific Error handling
+    await store.dispatch("logout");
+    await router.push("/");
+    await store.dispatch("unsetLoading");
+  }
+  try {
+    const response = await ApiService.get("/auth/users/me/");
+    await store.dispatch("setUser", response.data);
+  } catch {
+    // TODO: Specific Error handling
+    await store.dispatch("logout");
+    await router.push("/");
+    await store.dispatch("unsetLoading");
+  }
+  await router.push("/main");
+  await store.dispatch("unsetLoading");
+});
+</script>
 
 <style scoped lang="sass"></style>
