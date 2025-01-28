99405489b575b2dbc033c75d68083d7e39d85caa
mlikayoussef <youssefmlika.lac2@gmail.com>
2025-01-28 15:56:06 +0100

    setting last month to default

--- src/components/MonthSwitcher.vue
+++ src/components/MonthSwitcher.vue
@@ -1,47 +1,54 @@
-<script setup>
-import { computed, reactive } from "vue";
-import { addMonths, subMonths, format } from "date-fns";
-import { mdiChevronLeft, mdiChevronRight } from "@mdi/js";
-const icons = reactive({
-  prev: mdiChevronLeft,
-  next: mdiChevronRight
-});
-const model = defineModel({ type: Date });
-function goPrev() {
-  model.value = subMonths(model.value, 1);
-}
-function goNext() {
-  model.value = addMonths(model.value, 1);
-}
+<script setup>  
+import { computed, reactive, onMounted } from "vue";  
+import { addMonths, subMonths, format } from "date-fns";  
+import { mdiChevronLeft, mdiChevronRight } from "@mdi/js";  
 
-const formattedDate = computed(() => {
-  return format(model.value, "MMMM yyyy");
-});
-</script>
+const icons = reactive({  
+  prev: mdiChevronLeft,  
+  next: mdiChevronRight  
+});  
 
-<template>
-  <div
-    style="
-      min-width: 300px;
-      min-height: 50px;
-      display: flex;
-      justify-content: center;
-    "
-  >
-    <v-btn
-      class="justify-center"
-      variant="text"
-      :icon="icons.prev"
-      @click="goPrev"
-    >
-    </v-btn>
-    <v-spacer />
-    <span style="align-self: center">
-      {{ formattedDate }}
-    </span>
-    <v-spacer />
-    <v-btn variant="text" :icon="icons.next" @click="goNext"></v-btn>
-  </div>
+const model = reactive({ value: new Date() });  
+
+function setToLastMonth() {  
+  model.value = subMonths(new Date(), 1);  
+}  
+function goPrev() {  
+  model.value = subMonths(model.value, 1);  
+}  
+function goNext() {  
+  model.value = addMonths(model.value, 1);  
+}  
+
+const formattedDate = computed(() => {  
+  return format(model.value, "MMMM yyyy");  
+});  
+
+onMounted(() => {  
+  setToLastMonth();  
+});  
+</script>  
+
+<template>  
+  <div  
+    style="  
+      min-width: 300px;  
+      min-height: 50px;  
+      display: flex;  
+      justify-content: center;  
+    "  
+  >  
+    <v-btn  
+      class="justify-center"  
+      variant="text"  
+      :icon="icons.prev"  
+      @click="goPrev"  
+    ></v-btn>  
+    <v-spacer />  
+    <span style="align-self: center">  
+      {{ formattedDate }}  
+    </span>  
+    <v-spacer />  
+    <v-btn variant="text" :icon="icons.next" @click="goNext"></v-btn>  
+  </div>  
 </template>
-
-<style scoped lang="sass"></style>
